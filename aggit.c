fff8d12a78cf53349d41932bc8b3e562249a97fd
msouu <msouu.jp@gmail.com>
2018-10-27 23:16:29 +0900

    edit indent.

--- aggit.c
+++ aggit.c
@@ -4,40 +4,47 @@
 #include <err.h>
 #define BUF 256
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: aggit <path/to/repo>\n");
         return 1;
     }
 
-    FILE	*fp;
-	char	buf[BUF];
-	char	cmdline[256];
-    int     add = 0;
-    int     remove = 0;
+    FILE *fp;
+    char buf[BUF];
+    char cmdline[256];
+    int add = 0;
+    int remove = 0;
 
-	char *cmd1 = "cd ";
+    char *cmd1 = "cd ";
     char *cmd2 = " && git diff HEAD^";
 
     strcpy(cmdline, cmd1);
     strcat(cmdline, argv[1]);
     strcat(cmdline, cmd2);
 
-	if ( (fp=popen(cmdline,"r")) ==NULL) {
-		err(EXIT_FAILURE, "%s", cmdline);
-	}
-	while(fgets(buf, BUF, fp) != NULL) {
-        if (buf[0] == '+') {
+    if ((fp = popen(cmdline, "r")) == NULL)
+    {
+        err(EXIT_FAILURE, "%s", cmdline);
+    }
+    while (fgets(buf, BUF, fp) != NULL)
+    {
+        if (buf[0] == '+')
+        {
             add++;
-        } else if (buf[0] == '-') {
+        }
+        else if (buf[0] == '-')
+        {
             remove++;
         }
-	}
-	(void) pclose(fp);
+    }
+    (void)pclose(fp);
 
     printf("add: %d\n", add);
     printf("remove: %d\n", remove);
 
-	exit (EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
